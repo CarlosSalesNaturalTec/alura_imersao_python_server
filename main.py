bb122f052d7ed12608dd85bb3da20878e6eba6e2
Carlos Sales <carlossalesti@gmail.com>
2024-05-10 00:21:49 -0300

    Implementa download de arquivo de audio do WhatsAPp salvando o mesmo localmente

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()  
 
+# Variáveis de ambiente
+my_api_key = os.environ.get("API_KEY")                        # Gemini
+system_instruction =  os.environ.get("SYSTEM_INSTRUCTIONS")   # Gemini
+url_base = os.environ.get("URL_BASE")   # WhatsApp Cloud API
+token = os.environ.get("TOKEN")         # WhatsApp Cloud API
+
 # Parâmetros do modelo
 generation_config = {
   "temperature": 1,
@@ -33,8 +39,6 @@ safety_settings = [
     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
   },
 ]
-my_api_key = os.environ.get("API_KEY") 
-system_instruction =  os.environ.get("SYSTEM_INSTRUCTIONS")
 
 # Inicia modelo
 genai.configure(api_key=my_api_key)
@@ -48,36 +52,45 @@ app = Flask(__name__)
 # Endpoint POST para receber dados do webhook
 @app.route("/webhook", methods=["POST"])
 def webhook():   
-    # obtem mensagem da WhatsApp Cloud API
+    # obtem mensagem enviada pelo usuário (WhatsApp Cloud API)
     data = request.json
     if data.get("entry") and data["entry"][0].get("changes"):
         change = data["entry"][0]["changes"][0]
         if change.get("value") and change["value"].get("messages"):
             message = change["value"]["messages"][0]
-            id_message = message.get("id")
             tel = message.get("from")
-            timestamp = message.get("timestamp")
             type_message = message.get("type")
 
             if type_message == "text":
                 body_message = message.get("text").get("body")
             elif type_message == "button":
                 body_message = message.get("button").get("text")
-            else:
-                body_message = f"Mensagem do Tipo: {type_message}"
-                resposta = f"Mensagem recebida de {tel}: {body_message}"
-
+            elif type_message == "audio":
+                # obtem URL do audio
+                id_media = message.get("audio").get("id")                   
+                url_media = get_url_media(id_media)   
+                if url_media != "Erro":
+                  # faz o download do audio em formato binário
+                  media = download_media(url_media)             
+                  envia_msg_texto(tel, f"ID_media: {id_media} - URL da mídia: {url_media} - Download: {media}") 
+                  return jsonify({"status": "Ok"}), 200  
+                else:
+                  envia_msg_texto(tel, "Não foi possível obter URL do Audio") 
+                  return jsonify({"status": "Ok"}), 200                                    
+            else:               
+                resposta = f"Desculpe ainda não fui programado para analisar mensagens do tipo: {type_message}. Envie somente Texto ou Áudio"
+                envia_msg_texto(tel, resposta)
+                return jsonify({"status": "Ok"}), 200
+ 
     # envia mensagem para ser processada pela IA
     convo = model.start_chat(history= [])
     convo.send_message(body_message)
     resposta = convo.last.text
 
     # envia resposta de volta para o usuário através da WhatsApp Cloud API
-    response = envia_msg_texto(tel, resposta)
-    if response == True:
-      return jsonify({"status": "Ok"}), 200
-    else:
-      return jsonify({"status": "Erro ao enviar resposta pelo WhatsApp"}), 400
+    envia_msg_texto(tel, resposta)
+    return jsonify({"status": "Ok"}), 200
+
 
 # Endpoint GET para validação do webhook junto a WhatsApp Cloud API
 @app.route("/webhook", methods=["GET"])
@@ -95,6 +108,8 @@ def verify_webhook():
     else:
         return "Invalid request", 400
 
+
+# Envia mensagem de texto para a WhatsApp Cloud API
 def envia_msg_texto(tel, text_response):
     
     url_base = os.environ.get("URL_BASE") 
@@ -124,12 +139,42 @@ def envia_msg_texto(tel, text_response):
         json_response = response.json()
         if json_response.get("messages") and json_response["messages"][0].get("id"):            
             return True  # Indica sucesso
-        else:
-            print("Erro: retorno esperado não recebido.")
     else:
-        print(f"Erro ao enviar mensagem. Status code: {response.status_code}")
+        return False  # Indica falha        
 
-    return False  # Indica falha        
 
+# Obtem URL do audio enviado pela WhatsApp Cloud API
+def get_url_media(id_media):    
+    url = f"{url_base}/{id_media}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json().get("url")
+    else:
+        return "Erro"
+
+
+# Realiza o Download da midia (audio/video) enviada
+def download_media(url_media):    
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get(url_media, headers=headers, stream=True)
+        response.raise_for_status()  # Verifica se a requisição foi bem-sucedida
+
+        # Salva o arquivo
+        with open("audio.ogg", "wb") as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+        return "Ok"
+    except requests.exceptions.RequestException as e:
+        return f"Erro: {e}"
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
